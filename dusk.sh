db651aaf11318a1eb7996c6d1cc99a287617d749
Andrew Bolster <me@andrewbolster.info>
2012-11-05 12:20:21 +0000

    half way through integrating robertos sunset patches into dusk, got as far as verifying NSMIRACLE testing, looking good though.

--- dusk.sh
+++ dusk.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
+# Best guess for a working version
+VERSION=35
+#Present script path
+MYDIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #Check Package Requirements
-PACKAGES="libx11-dev libxt-dev gfortran subversion"
+PACKAGES="libx11-dev libxt-dev gfortran subversion automake1.10"
 dpkg -s $PACKAGES
 if [[ $? -ne 0 ]]; then
   echo "Some Libraries are not installed. Lets fix that shall we?"
@@ -88,10 +92,27 @@ vercomp () {
 # Generate Locale
 mkdir -p ${PREFIX}
 
+rm /etc/ld.so.conf.d/sunset.conf /etc/profile.d/sunset.conf
+
+echo >> /etc/ld.so.conf.d/sunset.conf << END
+$PREFIX/ns-allinone-$NSVER/otcl-$OTCLVER
+$PREFIX/ns-allinone-$NSVER/lib
+$PREFIX/lib
+END
+echo >> /etc/profile.d/sunset.conf << END
+export PATH="$PREFIX/ns-allinone-$NSVER/bin:$PREFIX/ns-allinone-$NSVER/tcl$TCLVER/unix:$PREFIX/ns-allinone-2.35/tk$TKVER/unix:$PATH"
+export TCL_LIBRARY="$PREFIX/ns-allinone-$NSVER/tcl$TCLVER/library,$TCL_LIBRARY"
+export SUNSET_PATH="$PREFIX/SUNSET_v$SUNSERVER"
+END
+
 # Get NS Allinone and Install
 if [ ! -x $PREFIX/ns-allinone-$NSVER/bin/ns ]; then 
   if [ ! -f $PREFIX/ns-ai1-$NSVER.tgz ]; then
     wget http://downloads.sourceforge.net/project/nsnam/allinone/ns-allinone-$NSVER/ns-allinone-$NSVER.tar.gz -O $PREFIX/ns-ai1-$NSVER.tgz 
+    if [ $? -ne 0 ]; then
+      echo "FUCKED!"
+      exit 1
+    fi
   fi  
   rm -rf $PREFIX/ns-allinone-$NSVER
   tar -xzf $PREFIX/ns-ai1-$NSVER.tgz -C $PREFIX
@@ -170,22 +191,39 @@ HERE
   cd $PREFIX/ns-allinone-$NSVER/
     
   ./install || exit 1
+  ldconfig
 fi
 if [ ! -x $PREFIX/ns-allinone-$NSVER/bin/ns ]; then
   echo "Can't find ns executable after ns creation; installation probably failed in some weird and wonderful way..."
   exit 1
 fi
 # NS-Miracles 
-if [ ! -d $PREFIX/nsmiracle-dev ]; then
-  svn co --username nsmiracle-dev-guest --password nsmiracleguest https://telecom.dei.unipd.it:/tlcrepos/nsmiracle-dev/trunk $PREFIX/nsmiracle-dev
-  cd $PREFIX/nsmiracle-dev/main/
+if [ ! -d $PREFIX/nsmiracle-trunk ]; then
+  svn co --username nsmiracle-dev-guest --password nsmiracleguest https://telecom.dei.unipd.it:/tlcrepos/nsmiracle-dev/trunk $PREFIX/nsmiracle-trunk
+  # Patching based on Roberto's November work
+  cd $PREFIX/nsmiracle-trunk
+    for patchfile in $MYDIR/patches/nsmiracle-trunk-fix-*.patch; 
+      do patch -N --ignore-whitespace -p1 < $patchfile; 
+    done
+  cd $PREFIX/nsmiracle-trunk/main/
+  patch -p 1 --ignore-whitespace << HERE
+--- main/samples/dei80211mr_infrastruct_plus_wired_voip.tcl.old  2012-11-05 12:15:41.299813183 +0000
++++ main/samples/dei80211mr_infrastruct_plus_wired_voip.tcl  2012-11-05 12:13:55.199816505 +0000
+@@ -123,7 +123,7 @@
+ dynlibload MiracleWirelessCh ../wirelessch/.libs/
+ dynlibload MiraclePhy802_11 ../phy802_11/.libs/
+ dynlibload MiracleMac802_11 ../mac802_11/.libs/
+-dynlibload dei80211mr
++dynlibload dei80211mr ../../../ns-allinone-$NSVER/lib/
+HERE
   ./autogen.sh
   ./configure --with-ns-allinone=$PREFIX/ns-allinone-${NSVER} --prefix=$PREFIX --disable-static --with-dei80211mr=$PREFIX/ns-allinone-${NSVER}/dei80211mr-${DEI80211MRVER}
   make && make install || (make clean && exit 1)
+  ldconfig
 fi
 
 #Test must be run in the samples director due to local dependencies
-cd $PREFIX/nsmiracle-dev/main/samples/
+cd $PREFIX/nsmiracle-trunk/main/samples/
 if [ -x $PREFIX/ns-allinone-$NSVER/bin/ns ]; then
   $PREFIX/ns-allinone-$NSVER/bin/ns dei80211mr_infrastruct_plus_wired_voip.tcl
 else
@@ -203,6 +241,7 @@ if [ ! -d ${PREFIX}/WOSS/at ]; then
   sed -i "s|/home/porter/at|/$PREFIX/WOSS/at|g" Makefile
   make clean
   make install || exit 1
+  ldconfig
 fi
 
 if [ ! -d $PREFIX/WOSS/woss ]; then
@@ -212,38 +251,34 @@ if [ ! -d $PREFIX/WOSS/woss ]; then
   ./autogen.sh
   ./configure --with-ns-allinone=$PREFIX/ns-allinone-${NSVER} --prefix=$PREFIX --with-pthread --with-nsmiracle=$PREFIX/lib
   make && make install || (make clean && exit 1)
+  ldconfig
 fi
 
 #SUNSET
 if [ ! -d $PREFIX/SUNSET_v$SUNSETVER ]; then
-  mkdir $PREFIX/SUNSET
   wget -O - "http://reti.dsi.uniroma1.it/UWSN_Group/framework/download/SUNSET_v$SUNSETVER.tar.gz" | tar -xzf - -C $PREFIX/
   cd $PREFIX/SUNSET_v$SUNSETVER/
+  patch --ignore-whitespace -p1 < $MYDIR/patches/patch_sunset_core.patch
   sed -i "s|NS_PATH=\"/home/example/\"|NS_PATH=\"$PREFIX/ns-allinone-$NSVER/\"|g" install_all.sh
-  sed -i "s|MIRACLE_PATH=\"/home/example/\"|MIRACLE_PATH=\"$PREFIX/nsmiracle-dev/main/\"|g" install_all.sh
+  sed -i "s|MIRACLE_PATH=\"/home/example/\"|MIRACLE_PATH=\"$PREFIX/nsmiracle-trunk/main/\"|g" install_all.sh
   ./install_all.sh || (make clean && exit 1)
-  #Clean up the bashrc messing
-  sed -i "/SUNSET_PATH/d" ~/.bashrc
   sed -i "s|pathMiracle \"insert_miracle_libraries_path_here\"|pathMiracle \"$PREFIX/lib\"|g" $PREFIX/SUNSET_v${SUNSETVER}/samples/*.tcl
   sed -i "s|pathWOSS \"insert_woss_libraries_path_here\"|pathWOSS \"$PREFIX/WOSS/\"|g" $PREFIX/SUNSET_v${SUNSETVER}/samples/*.tcl
   sed -i "s|pathSUNSET \"insert_sunset_libraries_path_here\"|pathSUNSET \"$PREFIX/lib\"|g" $PREFIX/SUNSET_v${SUNSETVER}/samples/*.tcl
+
+  #SUNSET Addon
+  wget -O - "http://reti.dsi.uniroma1.it/UWSN_Group/framework/download/SUNSETAddOn_v1.0.tar.gz" | tar -xzf - -C $PREFIX/SUNSET_v$SUNSETVER --strip-components 1
+  patch --ignore-whitespace -p1 < $MYDIR/patches/patch_sunset_addon.patch
+  sed -i "s|NS_PATH=\"/home/example/\"|NS_PATH=\"$PREFIX/ns-allinone-$NSVER/\"|g" install_sunset_addon.sh
+  sed -i "s|MIRACLE_PATH=\"/home/example/\"|MIRACLE_PATH=\"$PREFIX/nsmiracle-trunk/main/\"|g" install_sunset_addon.sh 
+  sed -i "s|SUNSET_PATH=\"\$CURRDIR\"|NS_PATH=\"$PREFIX/SUNSET_v${SUNSETVER}\"|g" install_sunset_addon.sh
+  ./install_sunset_addon.sh || (make clean && exit 1)
+
   #Test must be run in the samples director due to local dependencies
   cd $PREFIX/SUNSET_v$SUNSETVER/samples/
   $PREFIX/ns-allinone-$NSVER/bin/ns runSimulationBellhop.tcl
 fi
 
-rm /etc/ld.so.conf.d/sunset.conf /etc/profile.d/sunset.conf
-
-echo >> /etc/ld.so.conf.d/sunset.conf << END
-$PREFIX/ns-allinone-$NSVER/otcl-$OTCLVER
-$PREFIX/ns-allinone-$NSVER/lib
-$PREFIX/lib
-END
-echo >> /etc/profile.d/sunset.conf << END
-export PATH="$PREFIX/ns-allinone-$NSVER/bin:$PREFIX/ns-allinone-$NSVER/tcl$TCLVER/unix:$PREFIX/ns-allinone-2.35/tk$TKVER/unix:$PATH"
-export TCL_LIBRARY="$PREFIX/ns-allinone-$NSVER/tcl$TCLVER/library,$TCL_LIBRARY"
-export SUNSET_PATH="$PREFIX/SUNSET_v$SUNSERVER"
-END
-
-
+#Clean up the bashrc messing
+sed -i "/SUNSET_PATH/d" ~/.bashrc
 
