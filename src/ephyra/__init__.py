087deefe6d88795766406df7d40936391b370f0c
Andrew Bolster <me@andrewbolster.info>
2012-11-20 13:32:25 +0000

    PyCharm integration, as well as finally sorting out the expanding plot + slider control. Committing because the last time I walked away from this, wiggling the slider froze the machine, and of course I was working under /run/shm, like a genius...

--- src/ephyra/__init__.py
+++ src/ephyra/__init__.py
@@ -20,13 +20,7 @@ _ROOT=os.path.abspath(os.path.dirname(__file__))
 
 class EphyraFrame(wx.Frame):
     def __init__(self, *args, **kw):
-        super(EphyraFrame, self).__init__(
-                                            id=wx.ID_ANY,
-                                            pos=wx.DefaultPosition,
-                                            size=wx.Size( -1,-1 ),
-                                            style=wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL,
-                                            *args, **kw
-        )
+        super(EphyraFrame, self).__init__(*args, **kw)
         self.log = logging.getLogger(self.__module__)
         parser = argparse.ArgumentParser(description="GUI simulation and Analysis Suite for the Aietes framework")
 
@@ -38,33 +32,22 @@ class EphyraFrame(wx.Frame):
 
         self.args = parser.parse_args()
 
-        self.InitUI()
-
-        if self.args.data_file is not None:
-            if os.path.exists(self.args.data_file):
-                self.log.info("Loading data from %s"%self.args.data_file)
-                self.load_data(self.args.data_file)
-            else :
-                self.log.error("File Not Found: %s"%self.args.data_file)
-
-
         self.paused = True
+        self.t = 0
 
-    def InitUI(self):
         self.CreateMenuBar()
         panel = wx.Panel(self)
 
         self.plot_pnl = wx.Panel(self)
         self.plot_pnl.SetBackgroundColour(wx.BLACK)
-        self.pnl2 = wx.Panel(self)
+        self.control_pnl = wx.Panel(self)
 
-        self.time_slider = wx.Slider(self.pnl2, value=0, minValue=0, maxValue=1)
-        self.pause_btn = wx.Button(self.pnl2, label="Pause")
-        self.play_btn  = wx.Button(self.pnl2, label="Play")
-        self.forw_btn  = wx.Button(self.pnl2, label=">>")
-        self.back_btn  = wx.Button(self.pnl2, label="<<")
-        self.slider2 = wx.Slider(self.pnl2, value=1, minValue=0, maxValue=100, 
-            size=(120, -1))
+        self.time_slider = wx.Slider(self.control_pnl, value=1, minValue=0, maxValue=1)
+        self.pause_btn = wx.Button(self.control_pnl, label="Pause")
+        self.play_btn  = wx.Button(self.control_pnl, label="Play")
+        self.forw_btn  = wx.Button(self.control_pnl, label=">>")
+        self.back_btn  = wx.Button(self.control_pnl, label="<<")
+        self.slider2 = wx.Slider(self.control_pnl, value=1, minValue=0, maxValue=100, size=(120, -1))
 
         self.Bind(wx.EVT_SCROLL, self.on_time_slider, self.time_slider)
         self.Bind(wx.EVT_BUTTON, self.on_pause_btn, self.pause_btn)
@@ -86,20 +69,29 @@ class EphyraFrame(wx.Frame):
 
         vbox.Add(hbox1, flag=wx.EXPAND|wx.BOTTOM, border=10)
         vbox.Add(hbox2, proportion=1, flag=wx.EXPAND)
-        self.pnl2.SetSizer(vbox)
+        self.control_pnl.SetSizer(vbox)
 
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.plot_pnl, proportion=1, flag=wx.EXPAND)
-        sizer.Add(self.pnl2, flag=wx.EXPAND|wx.BOTTOM|wx.TOP, border=10)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.plot_pnl, proportion=1, flag=wx.EXPAND)
+        self.sizer.Add(self.control_pnl, flag=wx.EXPAND|wx.BOTTOM|wx.TOP, border=10)
+
+        self.Bind(wx.EVT_SIZE, self.on_resize)
 
         self.SetMinSize((800, 600))
         self.CreateStatusBar()
-        self.SetSizer(sizer)
+        self.SetSizer(self.sizer)
 
         self.SetSize((350, 200))
         self.SetTitle('Player')
         self.Centre()
         self.Show(True)
+
+        if self.args.data_file is not None:
+            if os.path.exists(self.args.data_file):
+                self.log.info("Loading data from %s"%self.args.data_file)
+                self.load_data(self.args.data_file)
+            else :
+                self.log.error("File Not Found: %s"%self.args.data_file)
 
     def CreateMenuBar(self):
 
@@ -151,11 +143,42 @@ class EphyraFrame(wx.Frame):
         self.axes.set_zlabel('Z')
 
     def redraw_plot(self):
-        for n,line in enumerate(lines):
-            (xs,ys,zs)=data.trail_of(n, self.t)
+        for n,line in enumerate(self.lines):
+            (xs,ys,zs)=self.data.trail_of(n, self.t)
             line.set_data(xs,ys)
             line.set_3d_properties(zs)
-            line.set_label(data.names[n])
+            line.set_label(self.data.names[n])
+
+        self.canvas.draw()
+
+    def resize_panel(self):
+        plot_size = self.sizer.GetChildren()[0].GetSize()
+        self.plot_pnl.SetSize(plot_size)
+        self.canvas.SetSize(plot_size)
+        self.fig.set_size_inches( float( plot_size[0])/self.fig.get_dpi(),
+                                  float( plot_size[0])/self.fig.get_dpi())
+
+
+
+    def load_data(self, data_file):
+        """ Load an external data file for plotting and navigation
+
+        :param data_file: Aietes generated data for processing
+        :type data_file: str
+
+        Configures Plot area and adjusts control area
+        """
+
+        self.data = DataPackage(data_file)
+        self.init_plot()
+        self.log.debug("Successfully loaded data from %s, containg %d nodes over %d seconds"%(
+            self.data.title,
+            self.data.n,
+            self.data.tmax
+            )
+        )
+        self.time_slider.SetRange(0,self.data.tmax)
+        self.time_slider.SetValue(self.data.tmax)
 
     def on_close(self, event):
         dlg = wx.MessageDialog(self,
@@ -181,25 +204,6 @@ class EphyraFrame(wx.Frame):
             data_path = data_path[0]
             self.load_data(data_path)
 
-    def load_data(self, data_file):
-        """ Load an external data file for plotting and navigation
-
-        :param data_file: Aietes generated data for processing
-        :type data_file: str
-
-        Configures Plot area and adjusts control area
-        """
-
-        self.data = DataPackage(data_file)
-        self.init_plot()
-        self.log.debug("Successfully loaded data from %s, containg %d nodes over %d seconds"%(
-                        self.data.title,
-                        self.data.n,
-                        self.data.tmax
-                        )
-        )
-        self.time_slider.SetRange(0,self.data.tmax)
-
     def on_pause_btn(self, event):
         self.paused = not self.paused
 
@@ -217,8 +221,15 @@ class EphyraFrame(wx.Frame):
         pass
 
     def on_time_slider(self,event):
+        event.Skip()
+        self.t = self.time_slider.GetValue()
+        self.log.debug("Slider: Setting time to %d"%self.t)
+        wx.CallAfter(self.redraw_plot)
         pass
 
+    def on_resize(self,event):
+        event.Skip()
+        wx.CallAfter(self.resize_panel)
 
 def main():
     app = wx.PySimpleApp()
