7a6983fb014883e3d11b155d6f9c9fb0db827b6d
Andrew Bolster <me@andrewbolster.info>
2013-11-12 21:21:34 +0000

    Turns out that there were some stupidly subtle bugs (RMS being the wrong way around, pow not working the same as **, off-by-one indexing, etc). Still not confirmed but here for sanity.

--- src/contrib/Plextek/uuv_position_drift_model/__init__.py
+++ src/contrib/Plextek/uuv_position_drift_model/__init__.py
@@ -9,34 +9,44 @@
 
  * Based on algorithmic work by Aled Catherall (Plextek Ltd)
 """
+from __future__ import division
 
 __author__ = 'Andrew Bolster, Aled Catherall'
 __license__ = "DEFCON705"
 __email__ = "me@andrewbolster.info"
 
 from numpy.random import randn, seed
-from numpy import pi, sqrt, exp, floor, mod, sin, cos, zeros
+from numpy import pi, sqrt, exp, mod, sin, cos, zeros
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+from matplotlib import pyplot as plt
+
+plt.rcParams['figure.figsize'] = 16, 12
 
 
-def testDriftExecution():
+def testDriftExecution(numloops=100):
     """
     Function to demonstrate and test the drift model of an unmanned underwater vehicle
     """
     # Establish Monte Carlo Properties (let Numpy handle this)
     seed()
-    numloops = 100
 
     # Mission Properties for test
     Duration = 8 * 60 * 60  # 8 hr mission (in s)
     dt = 1                  # time step
     x0 = 0.0                # initial position
     y0 = 0.0                # ''
-    n = Duration / dt       # n-steps
+    n = Duration // dt       # n-steps
     mission_types = {"straight line": 0, "big circle": 1, "lawn mower": 2}
     mission_type = mission_types['lawn mower']
 
+    first = True
+
     # MC loop iteration
     error_rms = zeros((numloops, n))
+    error_sum2 = zeros(n) #TODO I'm fairly certain that zeros() is faster than this.
 
     gyro_inits = zeros(numloops)
     dvl_inits = zeros(numloops)
@@ -44,10 +54,9 @@ def testDriftExecution():
         # Progress Monitor
         percent_done = (numloop * 100.0) / numloops
         if mod(percent_done, 10) == 0:
-            print("numloop:%f%%" % percent_done)
+            logging.info("numloop:%f%%" % percent_done)
 
         # Inititalise loop state (reset to zero mostly)
-        error_sum2 = x0 * zeros(n) #TODO I'm fairly certain that zeros() is faster than this.
         x_est = x0 * zeros(n)
         y_est = y0 * zeros(n)
         x_true = x0 * zeros(n)
@@ -57,9 +66,9 @@ def testDriftExecution():
 
         t = 0                           # Launch Time
 
-        bias = 0                        # Sensor Biases
-        bias_along = 0
-        bias_across = 0
+        bias = None                        # Sensor Biases (Delegate Default setting to DVL/GRYO fns
+        bias_along = None
+        bias_across = None
 
         gyro_inits[
             numloop] = Gyro_scale_rand_term = randn()  # Error onto Gryo bias: constant within loop but different between loops
@@ -70,7 +79,7 @@ def testDriftExecution():
         v_along, v_across, bearing_true = update_uuv_position(t, mission_type)
 
         # Kick off this run
-        for nStep in range(n):
+        for nStep in range(1, n):
             t += dt
 
             v_along_new, v_across_new, bearing_true_new = update_uuv_position(t, mission_type)
@@ -94,11 +103,31 @@ def testDriftExecution():
             y_true[nStep] = y_true[nStep - 1] + v_along * dt * sin(bearing_true) + v_across * dt * cos(bearing_true)
 
             # Record Error Marker
-            error_sum2[(nStep)] = error_sum2[(nStep - 1)] + (x_est[(nStep - 1)] - x_true[(nStep)]) ** 2 + (
-                y_est[(nStep)] - y_true[(nStep - 1)]) ** 2
+            error_sum2[nStep] += pow((x_est[(nStep - 1)] - x_true[nStep]), 2) + pow(
+                (y_est[nStep] - y_true[(nStep - 1)]), 2)
 
         # Record full loop error markers for test
-        error_rms[numloop] = sqrt(error_sum2 / numloops)
+        error_rms = sqrt(error_sum2 / numloops)
+        if False:
+            plt.figure(1)
+            if first:
+                plt.ion()
+                plt.show()
+                first = False
+            else:
+                #plt.clf()
+                pass
+            """
+            true_path = plt.plot(x_true, y_true, 'b')
+            est_path = plt.plot(x_est, y_est, 'r')
+            """
+            error_line = plt.plot(error_rms, 'b')
+            #error_src = plt.plot(error_sum2,'r')
+            plt.ylabel("x/m")
+            plt.xlabel("y/m")
+            plt.title("%d/%d" % (numloop, numloops))
+            plt.draw()
+
     return error_rms, gyro_inits, dvl_inits
 
 
@@ -122,7 +151,7 @@ def DVL(v_along_true, v_across_true, dt, bias_along, bias_across, DVL_scale_rand
     std_DVL_cn_ac = 0.001  # Across noise speed
     tc_DVL_cn = 1800       # Markov time constant
 
-    if count == 0: # Initialise bias state on first timestep of run
+    if bias_across is None and bias_along is None: # Initialise bias state on first timestep of run
         bias_along = std_DVL_cn_al * randn()
         bias_across = std_DVL_cn_ac * randn()
     else:
@@ -146,19 +175,14 @@ def GYRO(bearing_true_rate, dt, bias, Gyro_scale_rand_term, count):
     Gyro_scale_error = Gyro_scale_accuracy * Gyro_scale_rand_term
 
     # White noise characteristics
-    std_gyro_wn = 0.0025                    # deg / sqrt(hr)
-    std_gyro_wn = std_gyro_wn / 3600
-    std_gyro_wn = std_gyro_wn * pi / 180
-    q1 = std_gyro_wn ** 2
-    white_noise_term = sqrt(q1 * dt) * randn()
+    std_gyro_wn = (0.0025 / 3600) * (pi / 180)# rad / sqrt(hr)
+    white_noise_term = sqrt(pow(std_gyro_wn, 2) * dt) * randn()
 
     # Coloured / Biased term
-    std_gyro_cn = 0.0035                    # degrees / hr
-    std_gyro_cn /= 3600
-    std_gyro_cn = std_gyro_cn * pi / 180    # rad/s
+    std_gyro_cn = (0.0035 / 3600) * (pi / 180)# rad / hr
     tc_gyro_cn = 3600                       # Markov Time Constant
 
-    if count == 0: #  Initial bias state
+    if bias is None:                        # Initial bias state
         bias = std_gyro_cn * randn()
     else:
         bias = Markov_process(bias, tc_gyro_cn, std_gyro_cn, dt)
@@ -166,11 +190,29 @@ def GYRO(bearing_true_rate, dt, bias, Gyro_scale_rand_term, count):
     bearing_est_rate = bearing_true_rate * (1 + Gyro_scale_error) + bias + white_noise_term / dt
     return bearing_est_rate, bias
 
-# TODO Potential for memoisation as q-terms are near-static
-def Markov_process(bias, tc, std, dt):
-    ran = randn()
+
+def memoize(f):
+    """ Memoization decorator for a function taking one or more arguments. """
+
+    class memodict(dict):
+        def __getitem__(self, *key):
+            return dict.__getitem__(self, key)
+
+        def __missing__(self, key):
+            ret = self[key] = f(*key)
+            return ret
+
+    return memodict().__getitem__
+
+# TODO Potential for memoisation as q-terms are near-static (Same per run)
+@memoize
+def _partial_Markov(tc, std, dt):
     q = 2 * std * std / tc
-    bias = bias * exp(- dt / tc) + sqrt(0.5 * q * tc * (1 - exp(- 2 * dt / tc))) * ran
+    return exp(- dt / tc) + sqrt(0.5 * q * tc * (1 - exp(-2 * dt / tc)))
+
+
+def Markov_process(bias, tc, std, dt):
+    bias = bias * _partial_Markov(tc, std, dt) * randn()
     return bias
 
 
@@ -197,10 +239,10 @@ def update_uuv_position(t, mission_type):
             bearing_true_new = 0.0
         elif t < 27600:
             tt = t - 750
-            leg_no = floor(tt / t_leg)
+            leg_no = tt // t_leg
             parity = mod(leg_no, 2)
             leg_time = mod(tt, t_leg)
-            if leg_time < t_leg - t_turn:
+            if leg_time < (t_leg - t_turn):
                 bearing_true_new = 0 + pi * parity
             elif parity == 0:
                 bearing_true_new = (leg_time - t_leg + t_turn) * pi / t_turn
@@ -212,8 +254,19 @@ def update_uuv_position(t, mission_type):
                 bearing_true_new = pi + 0.4 * tt * pi / t_turn
             else:
                 bearing_true_new = - pi + 1.1
+    else:
+        raise NotImplementedError("Invalid Mission ID Used:%d" % mission_type)
     return v_along_new, v_across_new, bearing_true_new
 
 
 if __name__ == '__main__':
-    testDriftExecution()
+    from matplotlib import pyplot as plt
+
+    error_rms, gyro, dvl = testDriftExecution(numloops=10)
+    plt.ioff()
+    plt.figure(1)
+    plt.plot(error_rms, 'r')
+    plt.ylabel("error/m")
+    plt.xlabel("t/s")
+    plt.show()
+
