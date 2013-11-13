ccc9bfbff576c0dc40a8c802ecdc7eaa6e568a8a
Andrew Bolster <me@andrewbolster.info>
2013-11-13 20:11:20 +0000

    FECKING BODMAS IN THE FECKING MARKOV. All looks pretty good now. Need to work out how to package this as a regression testcase against the 500 model...

---
+++ src/contrib/Plextek/uuv_position_drift_model/UUV_position_drift_model.m
@@ -0,0 +1,269 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Copyright (c) 2013 Plextek Limited, Great Chesterford, England.
+%   All Rights Reserved
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   THIS FILE IS VERSION CONTROLLED USING SVN. DO NOT HAND EDIT THESE FIELDS
+%   ----------------------------------------------------------------------------
+%   $HeadURL$
+%   $1$
+%   $Aled Catherall$
+%   $Date$
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   File Description
+%   ================
+% 
+%   Script to predict the positional drift of a Unmanned underwater vehicle
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+1;
+function UUV_position_drift_model()
+clear all
+
+% Monte-Carlo Properties
+seed =1; % seed for random number generator
+rand('seed',seed)
+numloops = 100; % number of monte-carlo iterations
+
+% Mission properties
+Duration = 8*60*60;  % Mission length in seconds
+dt = 1;       % time step (1 second for intertial nav)
+x0 = 0;       % start coordinate
+y0 = 0;       % start y coordinate
+n = Duration/dt;  % number of time-steps in mission
+mission_type = 2; % options: 0 = straight line 1 = big circle, 2 = lawn mower pattern
+
+
+
+% monte-Carlo loop iteration
+    error_sum2 = x0*ones(1,n);  % Initialise estimated x position
+
+    for jj = 1:numloops    
+                
+      % initialise and reset these values each iteration of monte-carlo simulation     
+      x_est = x0*ones(1,n);  % Initialise  x position array
+      y_est = y0*ones(1,n);  % Initialise  y position array
+      x_true = x0*ones(1,n);  % Initialise estimated x position array
+      y_true = y0*ones(1,n);  % Initialise estimated y position array
+
+      bearing_true = 0; %initialise initial bearing
+      bearing_est = 0;  %initialise initialis eestimated bearing
+      
+  
+      
+      t = 0;            % initialise time
+      
+      % Bias terms for sensors   
+      bias = 0;         % initialise sensor biases
+      bias_along = 0;
+      bias_across = 0;
+      Gyro_scale_rand_term = rand(1,1);% Random Gaussian error onto bias - constant during a monte-carlo loop, but different between them
+      DVL_scale_rand_term  = rand(1,1);  %Starting value of scale error
+      
+      % update veocity and bearing of uuv
+      [v_along v_across bearing_true] = update_uuv_position(t,mission_type);
+      % Print to screen every 20th iteration of Monte-Carlo
+      if mod(jj,20) == 0
+        jj
+      end
+
+
+    %%%%   start the simulated run
+       for ii = 2:n
+ 
+            t = t + dt;
+            
+            [v_along_new v_across_new bearing_true_new] = update_uuv_position(t,mission_type);
+            
+            
+       
+            bearing_true_rate = (bearing_true_new - bearing_true)/dt;
+            bearing_true = bearing_true_new;
+            
+            % obtain parameters from Doppler Velocity Log and Gyro
+            [v_along_est , v_across_est, bias_along,bias_across] = DVL(v_along, v_across, s, dt, bias_along,bias_across,DVL_scale_rand_term,ii);
+            [bearing_est_rate, bias] = GYRO(bearing_true_rate, s, dt,bias,Gyro_scale_rand_term,ii);
+            
+         
+           
+            % update estimated bearing
+            bearing_est = bearing_est + bearing_est_rate*dt;
+            
+            % update estimated UUV position (first order Taylor finite difference)         
+            x_est(ii) = x_est(ii-1) + v_along_est*dt*cos(bearing_est) + v_across_est*dt*sin(bearing_est); 
+            y_est(ii) = y_est(ii-1) + v_along_est*dt*sin(bearing_est) + v_across_est*dt*cos(bearing_est);
+            
+            % update true position(first order Taylor finite difference) 
+            x_true(ii) = x_true(ii-1) + v_along*dt*cos(bearing_true) + v_across*dt*sin(bearing_true);
+            y_true(ii) = y_true(ii-1) + v_along*dt*sin(bearing_true) + v_across*dt*cos(bearing_true); 
+            
+            % calculate error
+            error_sum2(ii) = error_sum2(ii) + (x_est(ii)-x_true(ii)).^2+(y_est(ii)-y_true(ii)).^2;
+          
+            
+           
+       end % end of ii loop
+       % calculate rms error
+       error_rms = sqrt(error_sum2/numloops);
+     
+     
+   
+
+    end % end of jj loop
+    % Plot rms error
+     figure(1)
+     hold on
+     plot(dt*(1:n),error_rms,'-*k')
+     xlabel('time / s');
+     ylabel(' error / m');
+     figure(2)
+     plot(x_true,y_true,'b')
+     xlabel('x / m');
+     ylabel('y / m');
+     hold on
+     plot(x_est,y_est,'r')
+ 
+     
+end   % END FUNCITON
+
+
+function [v_along_est, v_across_est, bias_along, bias_across] = DVL(v_along_true, v_across_true, s, dt, bias_along,bias_across,DVL_scale_rand_term,count)
+  
+% Outputs estimated velocities and biases (along and across track)
+% this function should only be called once every second. !!!!!!!!!!!!!
+% the white noise is a per-measurment term, not a per root-hour
+  % scale
+  DVL_scale_accuracy = 1e-3;  
+  DVL_scale_error = DVL_scale_accuracy * DVL_scale_rand_term; % add the random term which is constant each monte-carlo loop
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % white noise
+  std_DVL_wn = 0.004;  % metres per second 
+  ran = rand(1,1);
+  white_noise_along =  std_DVL_wn*ran;  
+  ran = rand(1,1);
+  white_noise_across =  std_DVL_wn*ran; 
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   % Coloured noise (as a Markov process)
+  std_DVL_cn_al = 0.0041; % meters per second
+  std_DVL_cn_ac = 0.001;  % meters per second
+  tc_DVL_cn = 1800;     % time constant for markov process
+ 
+    if count == 2  % on first iteration of simulation, set the bias
+        ran = rand(1,1);  % generate random number (0,1)  = mean 0, std 1;
+        bias_along = std_DVL_cn_al*ran;
+        ran = rand(1,1);  % generate random number (0,1)  = mean 0, std 1;
+        bias_across = std_DVL_cn_ac*ran;
+    else
+     bias_along = Markov_process(bias_along,tc_DVL_cn,std_DVL_cn_al,dt,s);
+     bias_across = Markov_process(bias_across,tc_DVL_cn,std_DVL_cn_ac,dt,s);
+    end
+
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Add all errors to the true velocity to give estimated velocity
+  v_along_est =  v_along_true*(1+DVL_scale_error)  +  bias_along  + white_noise_along;
+  v_across_est = v_across_true*(1+DVL_scale_error) +  bias_across + white_noise_across;
+  
+ 
+end
+
+
+
+function [bearing_est_rate, bias] = GYRO(bearing_true_rate,s,dt,bias,Gyro_scale_rand_term,count)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Outputs estimated bearing rate and gyro bias
+% Gyroscope properties
+% you can call this function as often as you want
+
+% Scale error
+Gyro_scale_accuracy = 1e-4; 
+Gyro_scale_error = Gyro_scale_accuracy*Gyro_scale_rand_term;% add the random term which is constant each monte-carlo loop
+
+% random white noise
+ran = rand(1,1);  % generate random number (0,1)  = mean 0, std 1;
+std_gyro_wn = 0.0025;  % degrees per root hour
+std_gyro_wn = std_gyro_wn/3600;
+std_gyro_wn = std_gyro_wn * pi/180;
+q1 = std_gyro_wn^2;
+white_noise_term = sqrt(q1*dt)*ran;
+
+% Coloured noise (also known as bias)
+std_gyro_cn = 0.0035; % degrees per hour
+std_gyro_cn = std_gyro_cn/3600; % degrees per second
+std_gyro_cn = std_gyro_cn*pi/180; % radians per second
+tc_gyro_cn = 3600;    % time constant markov process
+if count == 2
+    ran = rand(1,1);
+    bias = std_gyro_cn*ran; % initial value at t = 0
+else
+    bias = Markov_process(bias,tc_gyro_cn,std_gyro_cn,dt,s); 
+end
+
+%calculate bearing rate
+bearing_est_rate = bearing_true_rate*(1+Gyro_scale_error) + bias  + white_noise_term/dt;
+end
+
+function [bias] = Markov_process(bias,tc,std,dt,s)
+% standard markov process
+ran = rand(1,1);
+q = 2*std*std/tc;
+bias = bias * exp(-dt/tc) + sqrt(0.5*q*tc*(1 - exp(-2*dt/tc)))*ran;
+
+end
+
+
+function [v_along_new v_across_new bearing_true_new] = update_uuv_position(t,mission_type);
+
+    switch mission_type
+    
+        case 0  % straight line
+            v_along_new = 1.5;  % metres per second
+            v_across_new = 0.0;
+            bearing_true_new = 0.0;
+
+        case 1  % big circle
+
+            v_along_new = 1.5;
+            v_across_new = 0.0;
+            bearing_true_new = 0.002*t; %500 seconds per %pi radians  
+
+        case 2   % lawn mower pattern
+            t_leg = 1500;
+            t_turn = 125;
+            v_along_new = 1.5;  % metres per second     
+            v_across_new = 0.0;
+            if t < 750      % traverse to survey site      
+
+                bearing_true_new = 0.0;
+            elseif t<27600
+
+                % start lawnmower pattern
+                % 900s straight line, then 180 degree turn taking 100 s
+                tt = t - 750;
+                leg_no = floor(tt/t_leg);
+                parity = mod(leg_no,2);
+                leg_time = mod(tt,t_leg);
+
+                if leg_time <t_leg - t_turn
+                    bearing_true_new = 0 + pi*parity; 
+
+                else
+                        if parity == 0
+                            bearing_true_new = (leg_time-t_leg+t_turn)*pi/t_turn;
+
+                        else
+                            bearing_true_new = pi-(leg_time-t_leg+t_turn)*pi/t_turn;
+                        end
+                end
+                
+            elseif t<27600+t_turn
+                tt = t - 27600;
+                 bearing_true_new = pi+0.4*tt*pi/t_turn;
+                 
+            else
+                bearing_true_new = -pi+1.1;
+            end
+            
+            
+            
+    end      
+  
+end
--- src/contrib/Plextek/uuv_position_drift_model/__init__.py
+++ src/contrib/Plextek/uuv_position_drift_model/__init__.py
@@ -16,14 +16,14 @@ __license__ = "DEFCON705"
 __email__ = "me@andrewbolster.info"
 
 from numpy.random import randn, seed
-from numpy import pi, sqrt, exp, mod, sin, cos, zeros
+from numpy import pi, sqrt, exp, mod, sin, cos, zeros, ones, multiply, float64, power as pow
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-from matplotlib import pyplot as plt
+from matplotlib.pyplot import figure, plot, xlabel, ylabel, show, ion, ioff, rcParams
 
-plt.rcParams['figure.figsize'] = 16, 12
+rcParams['figure.figsize'] = 16, 12
 
 
 def testDriftExecution(numloops=100):
@@ -31,44 +31,44 @@ def testDriftExecution(numloops=100):
     Function to demonstrate and test the drift model of an unmanned underwater vehicle
     """
     # Establish Monte Carlo Properties (let Numpy handle this)
-    seed()
+    # Numpy uses the Mersenne Twister MT19937ar, as prescribed in the MatLab
+    # randn presents a normally distributed (m=0,std=1) value
+    seed(1)
 
     # Mission Properties for test
-    Duration = 8 * 60 * 60  # 8 hr mission (in s)
+    duration = 8 * 60 * 60  # 8 hr mission (in s)
     dt = 1                  # time step
     x0 = 0.0                # initial position
     y0 = 0.0                # ''
-    n = Duration // dt       # n-steps
+    n = duration // dt       # n-steps
     mission_types = {"straight line": 0, "big circle": 1, "lawn mower": 2}
     mission_type = mission_types['lawn mower']
 
-    first = True
-
     # MC loop iteration
-    error_rms = zeros((numloops, n))
-    error_sum2 = zeros(n) #TODO I'm fairly certain that zeros() is faster than this.
+    error_sum2 = zeros(n)   # TODO I'm fairly certain that zeros() is faster than this.
 
     gyro_inits = zeros(numloops)
     dvl_inits = zeros(numloops)
+    ion()
     for numloop in range(numloops):
         # Progress Monitor
         percent_done = (numloop * 100.0) / numloops
         if mod(percent_done, 10) == 0:
-            logging.info("numloop:%f%%" % percent_done)
+            logging.info("numloop:%.3f%%" % percent_done)
 
         # Inititalise loop state (reset to zero mostly)
-        x_est = x0 * zeros(n)
-        y_est = y0 * zeros(n)
-        x_true = x0 * zeros(n)
-        y_true = y0 * zeros(n)
-        bearing_true = 0
-        bearing_est = 0
+        x_est = x0 * ones(n)
+        y_est = y0 * ones(n)
+        x_true = x0 * ones(n)
+        y_true = y0 * ones(n)
+        bearing_true = 0.0
+        bearing_est = 0.0
 
         t = 0                           # Launch Time
 
-        bias = None                        # Sensor Biases (Delegate Default setting to DVL/GRYO fns
-        bias_along = None
-        bias_across = None
+        bias = 0                        # Sensor Biases (Delegate Default setting to DVL/GRYO fns
+        bias_along = 0
+        bias_across = 0
 
         gyro_inits[
             numloop] = Gyro_scale_rand_term = randn()  # Error onto Gryo bias: constant within loop but different between loops
@@ -83,6 +83,7 @@ def testDriftExecution(numloops=100):
             t += dt
 
             v_along_new, v_across_new, bearing_true_new = update_uuv_position(t, mission_type)
+            # TODO make sure after verification that the _new values are used!
 
             bearing_true_rate = (bearing_true_new - bearing_true) / dt
             bearing_true = bearing_true_new
@@ -95,50 +96,35 @@ def testDriftExecution(numloops=100):
             bearing_est += bearing_est_rate * dt
 
             # Update estimated UUV Position
-            x_est[nStep] = x_est[nStep - 1] + v_along_est * dt * cos(bearing_est) + v_across_est * dt * sin(bearing_est)
-            y_est[nStep] = y_est[nStep - 1] + v_along_est * dt * sin(bearing_est) + v_across_est * dt * cos(bearing_est)
+            x_est[nStep] = x_est[nStep - 1] + v_along_est * dt * cos(bearing_est) \
+                           + v_across_est * dt * sin(bearing_est)
+            y_est[nStep] = y_est[nStep - 1] + v_along_est * dt * sin(bearing_est) \
+                           + v_across_est * dt * cos(bearing_est)
 
             # Update True UUV Position
-            x_true[nStep] = x_true[nStep - 1] + v_along * dt * cos(bearing_true) + v_across * dt * sin(bearing_true)
-            y_true[nStep] = y_true[nStep - 1] + v_along * dt * sin(bearing_true) + v_across * dt * cos(bearing_true)
+            x_true[nStep] = x_true[nStep - 1] + v_along * dt * cos(bearing_true) \
+                            + v_across * dt * sin(bearing_true)
+            y_true[nStep] = y_true[nStep - 1] + v_along * dt * sin(bearing_true) \
+                            + v_across * dt * cos(bearing_true)
 
             # Record Error Marker
-            error_sum2[nStep] += pow((x_est[(nStep - 1)] - x_true[nStep]), 2) + pow(
-                (y_est[nStep] - y_true[(nStep - 1)]), 2)
+            error_sum2[nStep] += pow((x_est[nStep] - x_true[nStep]), 2) \
+                                 + pow((y_est[nStep] - y_true[nStep]), 2)
 
-        # Record full loop error markers for test
-        error_rms = sqrt(error_sum2 / numloops)
-        if False:
-            plt.figure(1)
-            if first:
-                plt.ion()
-                plt.show()
-                first = False
-            else:
-                #plt.clf()
-                pass
-            """
-            true_path = plt.plot(x_true, y_true, 'b')
-            est_path = plt.plot(x_est, y_est, 'r')
-            """
-            error_line = plt.plot(error_rms, 'b')
-            #error_src = plt.plot(error_sum2,'r')
-            plt.ylabel("x/m")
-            plt.xlabel("y/m")
-            plt.title("%d/%d" % (numloop, numloops))
-            plt.draw()
+    error_rms = sqrt(error_sum2 / numloops)  # numloop+1 i.e. 1,2,3,4,5,...
 
+    # Record full loop error markers for test
     return error_rms, gyro_inits, dvl_inits
 
 
-def DVL(v_along_true, v_across_true, dt, bias_along, bias_across, DVL_scale_rand_term, count):
+def DVL(v_along_true, v_across_true, dt, bias_along, bias_across, DVL_scale_rand_term, nStep):
     """
     Outputs estimated velocities and biases (along and across track)
     Should only be called ONCE PER SECOND
     white noise is per-measurement not per root-hour
     """
     # Scaling factors
-    DVL_scale_accuracy = 0.001
+    DVL_scale_accuracy = 1e-3
     DVL_scale_error = DVL_scale_accuracy * DVL_scale_rand_term # Add random (but constant per run) term
 
     # White Noise conf
@@ -151,7 +137,7 @@ def DVL(v_along_true, v_across_true, dt, bias_along, bias_across, DVL_scale_rand
     std_DVL_cn_ac = 0.001  # Across noise speed
     tc_DVL_cn = 1800       # Markov time constant
 
-    if bias_across is None and bias_along is None: # Initialise bias state on first timestep of run
+    if nStep == 1:                        # Initial bias state
         bias_along = std_DVL_cn_al * randn()
         bias_across = std_DVL_cn_ac * randn()
     else:
@@ -165,29 +151,31 @@ def DVL(v_along_true, v_across_true, dt, bias_along, bias_across, DVL_scale_rand
     return v_along_est, v_across_est, bias_along, bias_across
 
 
-def GYRO(bearing_true_rate, dt, bias, Gyro_scale_rand_term, count):
+def GYRO(bearing_true_rate, dt, bias, Gyro_scale_rand_term, nStep):
     """
     Outputs estimated bearing rate and gyro bias
     Can be called as often as desired; non deterministic process
     """
     # Scaling Errors
-    Gyro_scale_accuracy = 0.0001
+    Gyro_scale_accuracy = 1e-4
     Gyro_scale_error = Gyro_scale_accuracy * Gyro_scale_rand_term
 
     # White noise characteristics
-    std_gyro_wn = (0.0025 / 3600) * (pi / 180)# rad / sqrt(hr)
+    std_gyro_wn = 0.0025
+    std_gyro_wn /= 3600
+    std_gyro_wn *= (pi / 180)# rad / s
     white_noise_term = sqrt(pow(std_gyro_wn, 2) * dt) * randn()
 
     # Coloured / Biased term
-    std_gyro_cn = (0.0035 / 3600) * (pi / 180)# rad / hr
+    std_gyro_cn = (0.0035 / 3600) * (pi / 180)# rad / s
     tc_gyro_cn = 3600                       # Markov Time Constant
 
-    if bias is None:                        # Initial bias state
+    if nStep == 1:                        # Initial bias state
         bias = std_gyro_cn * randn()
     else:
         bias = Markov_process(bias, tc_gyro_cn, std_gyro_cn, dt)
 
-    bearing_est_rate = bearing_true_rate * (1 + Gyro_scale_error) + bias + white_noise_term / dt
+    bearing_est_rate = bearing_true_rate * (1.0 + Gyro_scale_error) + bias + white_noise_term / dt
     return bearing_est_rate, bias
 
 
@@ -199,7 +187,7 @@ def memoize(f):
             return dict.__getitem__(self, key)
 
         def __missing__(self, key):
-            ret = self[key] = f(*key)
+            ret = self[key] = float64(f(*key))
             return ret
 
     return memodict().__getitem__
@@ -207,14 +195,15 @@ def memoize(f):
 # TODO Potential for memoisation as q-terms are near-static (Same per run)
 @memoize
 def _partial_Markov(tc, std, dt):
-    q = 2 * std * std / tc
-    return exp(- dt / tc) + sqrt(0.5 * q * tc * (1 - exp(-2 * dt / tc)))
+    q = multiply(2.0, pow(std, 2.0)) / tc
+    return sqrt(0.5 * q * tc * (1.0 - exp(multiply(-2.0, dt / tc))))
 
 
 def Markov_process(bias, tc, std, dt):
-    bias = bias * _partial_Markov(tc, std, dt) * randn()
+    bias = multiply(bias, exp(-dt / tc)) + _partial_Markov(tc, std, dt) * randn()
+    #q = multiply(2.0, pow(std, 2.0)) / tc
+    #bias = bias * exp(-dt / tc) + sqrt(0.5 * q * tc * (1.0 - exp(multiply(-2.0, dt / tc)))) * randn()
     return bias
-
 
 def update_uuv_position(t, mission_type):
     """
@@ -244,29 +233,28 @@ def update_uuv_position(t, mission_type):
             leg_time = mod(tt, t_leg)
             if leg_time < (t_leg - t_turn):
                 bearing_true_new = 0 + pi * parity
-            elif parity == 0:
-                bearing_true_new = (leg_time - t_leg + t_turn) * pi / t_turn
             else:
-                bearing_true_new = pi - (leg_time - t_leg + t_turn) * pi / t_turn
+                if parity == 0:
+                    bearing_true_new = (leg_time - t_leg + t_turn) * pi / t_turn
+                else:
+                    bearing_true_new = pi - (leg_time - t_leg + t_turn) * pi / t_turn
+        elif t < 27600 + t_turn:
+            tt = t - 27600
+            bearing_true_new = pi + 0.4 * tt * pi / t_turn
         else:
-            if t < 27600 + t_turn:
-                tt = t - 27600
-                bearing_true_new = pi + 0.4 * tt * pi / t_turn
-            else:
-                bearing_true_new = - pi + 1.1
+            bearing_true_new = - pi + 1.1
     else:
         raise NotImplementedError("Invalid Mission ID Used:%d" % mission_type)
     return v_along_new, v_across_new, bearing_true_new
 
 
 if __name__ == '__main__':
-    from matplotlib import pyplot as plt
 
     error_rms, gyro, dvl = testDriftExecution(numloops=10)
-    plt.ioff()
-    plt.figure(1)
-    plt.plot(error_rms, 'r')
-    plt.ylabel("error/m")
-    plt.xlabel("t/s")
-    plt.show()
+    ioff()
+    figure(1)
+    plot(error_rms, 'r')
+    ylabel("error/m")
+    xlabel("t/s")
+    show()
 
