2fe5209c80a476c773ee52910088d884390d5203
Andrew Bolster <me@andrewbolster.info>
2016-04-26 17:21:06 +0100

    sassen frassen pathen...testen...

--- scripts/publication_scripts/setup_TrustCom16.py
+++ scripts/publication_scripts/setup_TrustCom16.py
@@ -48,8 +48,6 @@ def run_all_analysis_generation(results_path):
             if os.path.isfile(best_output_file):
                 print("Skipping: File already exists, consider deleting it {}".format(best_output_file))
             try:
-                with open(best_output_file) as f:
-                    f.write("")
                 feat_weights = categorise_dataframe(non_zero_rows(subset_d['data']).T)
                 if subset_d['metrics'] is not None:
                     best = best_of_all(feat_weights, trust_observations[subset_d['metrics']], par=par)
