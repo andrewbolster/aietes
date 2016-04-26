1b92c7467807e247baf3846ec68dc3f770207571
Andrew Bolster <me@andrewbolster.info>
2016-04-26 18:50:05 +0100

    I think this might actually work

--- scripts/publication_scripts/setup_TrustCom16.py
+++ scripts/publication_scripts/setup_TrustCom16.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import unittest
 import matplotlib.pylab as plt
+from shutil import copy
 import pandas as pd
 from tqdm import *
 from joblib import Parallel, delayed
@@ -12,6 +13,7 @@ from scipy.stats.stats import pearsonr
 from aietes.Tools import *
 
 results_path = "/home/bolster/src/aietes/results/Malicious Behaviour Trust Comparison-2015-07-20-17-47-53"
+json_path = "/home/bolster/Dropbox/aietes_json_results"
 fig_basedir = "/home/bolster/src/thesis/Figures/"
 
 shared_h5_path = '/dev/shm/shared.h5'
@@ -37,16 +39,18 @@ def run_all_analysis_generation(results_path):
     with pd.get_store(shared_big_h5_path) as store:
         result = map(power_set_map, store.keys())
         powerset_list = filter(lambda t: t['type'] == 'feats', result)
+        random.shuffle(powerset_list)
         for d in powerset_list:
             d['data'] = store[d['dataset']]
 
     best_weight_valences_and_runs_for_metric_subset = {}
     with Parallel(n_jobs=-1) as par:
         for subset_d in tqdm(powerset_list): # This maps to a single powerset_shared.h5 dataset (comms_alt, etc)
-            best_output_file = '{}.json'.format(os.path.join(results_path,subset_d['dataset']))
+            best_output_file = '{}.json'.format(os.path.join(json_path,subset_d['dataset']))
 
             if os.path.isfile(best_output_file):
                 print("Skipping: File already exists, consider deleting it {}".format(best_output_file))
+                continue
             try:
                 feat_weights = categorise_dataframe(non_zero_rows(subset_d['data']).T)
                 if subset_d['metrics'] is not None:
@@ -57,7 +61,8 @@ def run_all_analysis_generation(results_path):
 
 
                 with open(best_output_file, 'w') as f:
-                    json.dump(best, cls=NumpyAwareJSONEncoder)
+                    json.dump(best, f, cls=NumpyAwareJSONEncoder)
+                copy(best_output_file, results_path)
             except:
                 print("Failed on {}".format(subset_d['dataset']))
                 raise
